2d987712179023da1b21f676c4cb1c23a44808dd
Nicholas Flintham <nick@flinny.org>
2015-10-20 11:32:45 +0100

    Add back Slim DeviceHandler
    Change-Id: I09523669fe06e9de425930f2244f9435088b7b15

--- m8-common.mk
+++ m8-common.mk
@@ -54,3 +54,7 @@ PRODUCT_PACKAGES += \
 # Variant linking script
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/releasetools/variant_script.sh:install/bin/variant_script.sh
+
+# Device Handler
+PRODUCT_PACKAGES += \
+    DeviceHandler
